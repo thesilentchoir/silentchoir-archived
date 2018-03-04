31a54d6c1800104fac227a626926020280ab2b70
ashachs <asachs@thoughtworks.com>
2018-03-04 16:23:15 -0500

    AS: trying to deploy to Heroku

---
+++ silentchoir/local_settings.py
@@ -0,0 +1,15 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'NAME': os.getenv('DATABASE_NAME'),
+    }
+}
+
+DEBUG = True
--- silentchoir/settings.py
+++ silentchoir/settings.py
@@ -126,5 +126,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
